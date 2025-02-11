557c624f36bee690c2abe1cfa6322ef2a1226d3f
sanjula pilapitiya <sanjulapilapitiya@hotmail.com>
2025-02-11 16:36:55 +0000

    refactored map to use props and emit, allows me to have dynamic info to be shown depending on what markers are selected

--- src/index.vue
+++ src/index.vue
@@ -2,12 +2,18 @@
     <div id="app">
       <h1>Travel Tracker</h1>
       <div class="map-container">
-        <Map />
+        <Map :locations="locations" @marker-selected="onMarkerSelected" />
       </div>
+      <hr>
+      <h2 v-if="locations = selectedLocation">
+        {{ selectedLocation.name }}
+      </h2>
+      <p v-if="selectedLocation?.desciption">{{ selectedLocation.desciption }}</p>
     </div>
   </template>
   
   <script>
+  import { ref } from 'vue';
   import Map from './map.vue';
   
   export default {
@@ -15,7 +21,49 @@
     components: {
       Map,
     },
-  };
+    setup() {
+        // Define an array of locations to display on the map
+        const locations = ref([
+            { lat: 51.5, lng: -0.09, name: 'England', desciption: 'I live here yo yo yo' },
+            { lat: 48.8566, lng: 2.3522, name: 'France' },
+            { lat: 53.34061, lng: -1.28162, name: 'Wales' },
+            { lat: 54.58333, lng: -5.93333, name: 'Northern Ireland' },
+            { lat: 52.52437, lng: 13.41053, name: 'Germany' },
+            { lat: 47.49801, lng: 19.03991, name: 'Hungary' },
+            { lat: 48.20849, lng: 16.37208, name: 'Austria' },
+            { lat: 41.89193, lng: 12.51133, name: 'Italy' },
+            { lat: 52.37403, lng: 4.88969, name: 'Netherlands' },
+            { lat: 59.33258, lng: 18.0649, name: 'Sweden' },
+            { lat: 55.67594, lng: 12.56553, name: 'Denmark' },
+            { lat: 64.13548, lng: -21.89541, name: 'Iceland' },
+            { lat: 41.38879, lng: 2.15899, name: 'Spain' },
+            { lat: 38.75382, lng: -9.23083, name: 'Portugal' },
+            { lat: 37.97945, lng: 23.71622, name: 'Greece' },
+            { lat: 50.08804, lng: 14.42076, name: 'Czech Republic' },
+            { lat: 46.20222, lng: 6.14569, name: 'Switzerland' },
+            { lat: 41.90236, lng: 12.45332, name: 'Vatican City' },
+            { lat: 30.06263, lng: 31.24967, name: 'Egypt' },
+            { lat: 33.58831, lng: -7.61138, name: 'Morocco' },
+            { lat: 6.93194, lng: 79.84778, name: 'Sri Lanka' },
+            { lat: 3.1412, lng: 101.68653, name: 'Malaysia' },
+            { lat: 1.28967, lng: 103.85007, name: 'Singapore' },
+            { lat: -6.21462, lng: 106.84513, name: 'Indonesia' },
+            { lat: 34.05223, lng: -118.24368, name: 'America' },
+        ]);
+
+        const selectedLocation = ref(null);
+
+        const onMarkerSelected = (location) => {
+        selectedLocation.value = location;
+        };
+
+        return {
+            locations,
+            selectedLocation,
+            onMarkerSelected,
+        };
+    }
+};
 </script>
   
 <style>
@@ -23,14 +71,36 @@
   margin: 0;
   padding: 0; /* reset margin/padding introduced in Import leaflet css in main.js */
 }
+h1 {
+    padding: 25px;
+    font-family: 'Papyrus', fantasy;
+    font-weight: 900;
+    font-size: 40px;
+    color: white;
+}
 #app {
     text-align: center;
     width: 100%;
-    background: linear-gradient(-45deg, #606c38, #283618,#fefae0, #dda15e, #bc6c25);
+    background: linear-gradient(-45deg, #fefae0, #dda15e, #bc6c25);
     background-size: 400% 400%;
     animation: gradient 30s ease infinite;
     height: 100vh;
 }
+.map-container {
+    width: 100%;
+    max-width: 900px;
+    height: 500px;
+    padding: 15px;
+    margin: 0 auto; /* Center the map container horizontally */
+}
+hr {
+  border: 1px solid #fefae0;
+  max-width: 75%;
+  text-align: center;
+  border-radius: 5px;
+  margin: auto;
+}
+
 @keyframes gradient {
     0% {
         background-position: 0% 50%;
@@ -43,12 +113,6 @@
     100% {
         background-position: 0% 50%;
     }
-}
-.map-container {
-    width: 100%;
-    max-width: 900px;
-    height: 500px;
-    margin: 0 auto; /* Center the map container horizontally */
 }
 </style>
   
--- src/map.vue
+++ src/map.vue
@@ -8,8 +8,13 @@
   
   export default {
     name: 'Map',
-    setup() {
+    props: {
+        locations: Array,
+    },
+    emits: ['marker-selected'],
+    setup(props, { emit }) {
       onMounted(() => {
+        console.log(props.locations);
         // Fix for missing marker icons
         delete L.Icon.Default.prototype._getIconUrl;
 
@@ -22,44 +27,25 @@
         // Initialize the map
         const map = L.map('map').setView([51.505, -0.09], 2); // Set the initial view (latitude, longitude, zoom level)
   
-        // Add OpenStreetMap tile layer (OSM tiles)
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                        '&copy; <a href="https://stadia.maps.io">Stadia Maps</a>, ' +
+                        '&copy; <a href="http://stamen.com">Stamen Design</a>',
+            maxZoom: 18
         }).addTo(map);
-  
-        // Define an array of locations to display on the map
-        const locations = [
-          { lat: 51.5, lng: -0.09, name: 'England' },
-          { lat: 48.8566, lng: 2.3522, name: 'France' },
-          { lat: 53.34061, lng: -1.28162, name: 'Wales' },
-          { lat: 54.58333, lng: -5.93333, name: 'Northern Ireland' },
-          { lat: 52.52437, lng: 13.41053, name: 'Germany' },
-          { lat: 47.49801, lng: 19.03991, name: 'Hungary' },
-          { lat: 48.20849, lng: 16.37208, name: 'Austria' },
-          { lat: 41.89193, lng: 12.51133, name: 'Italy' },
-          { lat: 52.37403, lng: 4.88969, name: 'Netherlands' },
-          { lat: 59.33258, lng: 18.0649, name: 'Sweden' },
-          { lat: 55.67594, lng: 12.56553, name: 'Denmark' },
-          { lat: 64.13548, lng: -21.89541, name: 'Iceland' },
-          { lat: 41.38879, lng: 2.15899, name: 'Spain' },
-          { lat: 38.75382, lng: -9.23083, name: 'Portugal' },
-          { lat: 37.97945, lng: 23.71622, name: 'Greece' },
-          { lat: 50.08804, lng: 14.42076, name: 'Czech Republic' },
-          { lat: 46.20222, lng: 6.14569, name: 'Switzerland' },
-          { lat: 41.90236, lng: 12.45332, name: 'Vatican City' },
-          { lat: 30.06263, lng: 31.24967, name: 'Egypt' },
-          { lat: 33.58831, lng: -7.61138, name: 'Morocco' },
-          { lat: 6.93194, lng: 79.84778, name: 'Sri Lanka' },
-          { lat: 3.1412, lng: 101.68653, name: 'Malaysia' },
-          { lat: 1.28967, lng: 103.85007, name: 'Singapore' },
-          { lat: -6.21462, lng: 106.84513, name: 'Indonesia' },
-          { lat: 34.05223, lng: -118.24368, name: 'America' },
-        ];
+
+        const customIcon = L.icon({
+            iconUrl: '/asset/pin.png',
+            iconSize: [20, 20],
+        });
   
         // Add markers to the map for each location
-        locations.forEach((location) => {
-          L.marker([location.lat, location.lng]).addTo(map)
-            .bindPopup(`<b>${location.name}</b>`);
+        props.locations.forEach((location) => {
+          L.marker([location.lat, location.lng], { icon: customIcon }).addTo(map)
+            .bindPopup(`<b>${location.name}</b>`)
+            .on('click', () => {
+                emit('marker-selected', location);
+            });
         });
       });
     },
